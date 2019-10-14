10f5014428df0572c50be2bfe8cd886416b238c1
asmi38 <alecsmith@live.com>
2019-10-15 03:47:22 +1100

    added player input form
    player input form for username added which changes state value, no functionality yet

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { FaUserFriends, FaFighterJet, FaTrophy } from 'react-icons/fa'
+import PropTypes from 'prop-types'
 
 function Instructions () {
   return (
@@ -19,18 +20,75 @@ function Instructions () {
         <li>
           <h3 className='header-sm'>Enter two Github users</h3>
           <FaTrophy className='bg-light' color='rgb(255, 215, 0)' size={140} />
-        </li>                
+        </li>
       </ol>
     </div>
   )
 }
 
+class PlayerInput extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      username: ''
+    }
+
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+  }
+  handleSubmit(event) {
+    event.preventDefault()
+
+    this.props.onSubmit(this.state.username)
+  }
+  handleChange(event) {
+    this.setState({
+      username: event.target.value
+    })
+  }
+
+  render () {
+    return (
+      <form className='column player' onSubmit={this.handleSubmit}>
+        <label htmlFor='username' className='player-label'>
+          {this.props.label}
+        </label>
+        <div className='row player-inputs'>
+          <input
+            type='text'
+            id='username'
+            className='input-light'
+            placeholder='github username'
+            autoComplete='off'
+            value={this.state.username}
+            onChange={this.handleChange}
+          />
+          <button
+            className='btn dark-btn'
+            type='submit'
+            disabled={!this.state.username}
+          >
+            Submit
+          </button>
+        </div>
+      </form>
+    )
+  }
+}
+
+PlayerInput.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+  label: PropTypes.string.isRequired
+}
 
 export default class Battle extends React.Component {
   render() {
     return (
       <React.Fragment>
         <Instructions />
+
+        <PlayerInput label="Label!" onSubmit={(value) => console.log('value!', value)}/>
       </React.Fragment>
     )
   }
