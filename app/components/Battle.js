3ea660e9bccffc7e64192bb5f36b327334237d49
asmi38 <alecsmith@live.com>
2019-10-16 20:05:42 +1100

    player preview and reset
    player preview function that accepts username and provides info, reset function included too

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { FaUserFriends, FaFighterJet, FaTrophy } from 'react-icons/fa'
+import { FaUserFriends, FaFighterJet, FaTrophy, FaTimesCircle } from 'react-icons/fa'
 import PropTypes from 'prop-types'
 
 function Instructions () {
@@ -42,6 +42,7 @@ class PlayerInput extends React.Component {
 
     this.props.onSubmit(this.state.username)
   }
+
   handleChange(event) {
     this.setState({
       username: event.target.value
@@ -84,6 +85,37 @@ PlayerInput.propTypes = {
   label: PropTypes.string.isRequired
 }
 
+function PlayerPreview ({ username, onReset, label }) {
+  return (
+    <div className='column player'>
+      <h3 className='player-label'>{label}</h3>
+      <div className='row bg-light'>
+        <div className='player-info'>
+          <img
+            className='avatar-small'
+            src={`https://github.com/${username}.png?size=200`}
+            alt={`Avatar for ${username}`}
+          />
+          <a
+            href={`https://github.com/${username}`}
+            className='link'>
+              {username}
+          </a>
+        </div>
+        <button className='btn-clear flex-center' onClick={onReset}>
+          <FaTimesCircle color='rgb(194, 57, 42)' size={26} />
+        </button>
+      </div>
+    </div>
+  )
+}
+
+PlayerPreview.propTypes = {
+  username : PropTypes.string.isRequired,
+  onReset: PropTypes.func.isRequired,
+  label: PropTypes.string.isRequired
+}
+
 export default class Battle extends React.Component {
   constructor(props) {
     super(props)
@@ -93,11 +125,18 @@ export default class Battle extends React.Component {
       playerTwo: null,
     }
     this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleReset = this.handleReset.bind(this)
   }
 
   handleSubmit(id, player) {
     this.setState({
       [id]: player
+    })
+  }
+
+  handleReset(id) {
+    this.setState({
+      [id]: null
     })
   }
 
@@ -111,19 +150,29 @@ export default class Battle extends React.Component {
         <div className='players-container'>
           <h1 className='center-text header-lg'>Players</h1>
           <div className='row space-around'>
-            {playerOne === null && (
-              <PlayerInput
-                label='Player One'
-                onSubmit={(player) => this.handleSubmit('playerOne', player)}
-              />
-            )}
+            {playerOne === null
+              ? <PlayerInput
+                  label='Player One'
+                  onSubmit={(player) => this.handleSubmit('playerOne', player)}
+                  />
+              : <PlayerPreview
+                  username={playerOne}
+                  label='Player One'
+                  onReset={() => this.handleReset('playerOne')}
+                />
+            }
 
-            {playerTwo === null && (
-              <PlayerInput
-                label='Player Two'
-                onSubmit={(player) => this.handleSubmit('playerTwo', player)}
-              />
-            )}
+            {playerTwo === null
+              ? <PlayerInput
+                  label='Player Two'
+                  onSubmit={(player) => this.handleSubmit('playerTwo', player)}
+                />
+              : <PlayerPreview
+                  username={playerTwo}
+                  label='Player Two'
+                  onReset={() => this.handleReset('playerTwo')}
+                />
+            }
           </div>
         </div>
       </React.Fragment>
